7de620ae01fb2f5c52073166bea014ce7ff4559f
rien100moi <vincent.chareunphol@gmail.com>
2018-08-11 15:53:58 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -9,7 +9,7 @@ class profile::ssh_server {
         ssh_authorized_key { 'root@master.puppet.vm':
                 ensure => present,
                 user => 'root',
-                type => 'id-rsa',
+                type => 'ssh-rsa',
                 key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDh5WfuX+kWt4An8roUPXIFdJdegB1EqreOm8Pe02ryqomgk8s3WCJYlfkNoIC4++b0VsJ2w0SJkOVGUw+plHVLtm9aWyyyLtje0YUoWMFu/VWxT1q/uhxtR9meo9w6OPDAdRNe1a4TO1YX+2o3xun+/HK9EeS9zulz3bFSt4TpN/zTczPgM4208siATLzoZriIrM25Qq51M1GN4jPrsifPK7a616jUiL7eiqxJzIVkA/EKobrnoC7bxWYF/6ySzUuFoWAyOlGi0uuISTiKDRHvgOjcpCSefkrMJN+Ys0tuzCPrZoxn70r9/01UXb2VmdH4pargQcnmZMTElILzKuf7',
         }
 }
